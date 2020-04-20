894848fac1b029a099e1e0d837bbd24e117b5a2e
yofanalfiatur <yofanalfiatur@gmail.com>
2020-04-20 18:24:21 +0700

    tambah Api response

--- app/src/main/java/com/example/projectpraktikum/service/ApiMain.java
+++ app/src/main/java/com/example/projectpraktikum/service/ApiMain.java
@@ -1,4 +1,20 @@
 package com.example.projectpraktikum.service;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class ApiMain {
+    private Retrofit retrofit;
+
+    public CountryRepository getApiCountry(){
+        String BASE_URL = "https://getfestivo.com/";
+        if (retrofit == null){
+            retrofit = new Retrofit
+                    .Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit.create(CountryRepository.class);
+    }
 }
--- app/src/main/java/com/example/projectpraktikum/service/CountryRepository.java
+++ app/src/main/java/com/example/projectpraktikum/service/CountryRepository.java
@@ -1,4 +1,11 @@
 package com.example.projectpraktikum.service;
 
+import com.example.projectpraktikum.model.country.CountryDiscoverResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface CountryRepository {
+    @GET("v1/holidays?api_key=87e8bec6-2695-4f86-a88c-4bb24858d408&country=US&year=2020&month=12&day=25")
+    Call<CountryDiscoverResponse> getCountryDiscover();
 }
