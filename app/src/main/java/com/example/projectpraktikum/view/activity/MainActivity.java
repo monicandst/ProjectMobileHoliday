115696bd5322e41384fa7f529c879efeecc625dc
yofanalfiatur <yofanalfiatur@gmail.com>
2020-04-20 12:13:05 +0700

    tambah class & packages

--- app/src/main/java/com/example/projectpraktikum/view/activity/MainActivity.java
+++ app/src/main/java/com/example/projectpraktikum/view/activity/MainActivity.java
@@ -1,16 +1,36 @@
 package com.example.projectpraktikum.view.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.graphics.Movie;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.projectpraktikum.R;
+import com.example.projectpraktikum.view.fragment.CountryFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
+    private Fragment selectedFragment = new CountryFragment();
+    private BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.btn_nav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case
+        }
+
+        return false;
     }
 }
