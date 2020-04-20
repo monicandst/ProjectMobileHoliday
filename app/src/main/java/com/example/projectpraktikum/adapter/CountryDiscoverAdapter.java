9a01bb71abbcfc99f5b7d75588d9327275c1d3d9
yofanalfiatur <yofanalfiatur@gmail.com>
2020-04-20 11:58:22 +0700

    tambah class & packages

---
+++ app/src/main/java/com/example/projectpraktikum/adapter/CountryDiscoverAdapter.java
@@ -0,0 +1,4 @@
+package com.example.projectpraktikum.adapter;
+
+public class CountryDiscoverAdapter {
+}
---
+++ app/src/main/java/com/example/projectpraktikum/view/viewmodel/CountryViewModel.java
@@ -0,0 +1,4 @@
+package com.example.projectpraktikum.view.viewmodel;
+
+public class CountryViewModel {
+}
